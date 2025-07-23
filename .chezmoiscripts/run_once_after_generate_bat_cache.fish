93956d127ef4d211d7352eb7cc37d67ad04d53f6
Anariel Teleri <218691229+AnarielTeleri@users.noreply.github.com>
2025-07-23 18:47:54 +1000

    Add Bat configuration

---
+++ .chezmoiscripts/run_once_after_generate_bat_cache.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/env fish
+
+# We need to build the cache for the Catppuccin theme to be usable.
+echo "Building Bat cache."
+
+bat cache --build
---
+++ private_dot_config/fish/conf.d/alias.fish
@@ -0,0 +1,2 @@
+# Replace common commands with newer ones with better features.
+alias cat bat
